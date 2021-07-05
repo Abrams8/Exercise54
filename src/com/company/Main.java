70350e14cfe31c0d055b9777d428bde0a7f5f6c0
Abrams <dima.abramchik@gmail.com>
2021-07-05 03:11:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Thread threadOne = new Thread(new MyRunnable());
+        Thread threadTwo = new Thread(new MyRunnable());
+        Thread threadThree = new Thread(new MyRunnable());
+        Thread threadFour = new Thread(new MyRunnable());
+        Thread threadFive = new Thread(new MyRunnable());
+
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
+        threadFour.start();
+        threadFive.start();
+    }
+}
---
+++ src/com/company/MyRunnable.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public synchronized void  run () {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+
+        File file = new File("C:\\Users\\Abrams\\IdeaProjects\\Exercise54\\src\\com\\company\\thread.txt");
+        try (FileWriter fileWriter = new FileWriter(file, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            for (int i = 0; i < array.length; i++) {
+            bufferedWriter.write(String.valueOf(array[i]));
+            bufferedWriter.write(" ");
+            }
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write into file");
+
+        }
+
+
+    }
+}
