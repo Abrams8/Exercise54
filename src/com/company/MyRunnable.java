90f99078ef13b2c6f2c8e78200285b89872b3cee
Abrams <dima.abramchik@gmail.com>
2021-07-05 03:13:02 +0300

    init

--- src/com/company/MyRunnable.java
+++ src/com/company/MyRunnable.java
@@ -8,25 +8,21 @@ import java.io.IOException;
 public class MyRunnable implements Runnable {
 
     @Override
-    public synchronized void  run () {
+    public synchronized void run() {
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 100);
         }
-
         File file = new File("C:\\Users\\Abrams\\IdeaProjects\\Exercise54\\src\\com\\company\\thread.txt");
         try (FileWriter fileWriter = new FileWriter(file, true);
              BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
             for (int i = 0; i < array.length; i++) {
-            bufferedWriter.write(String.valueOf(array[i]));
-            bufferedWriter.write(" ");
+                bufferedWriter.write(String.valueOf(array[i]));
+                bufferedWriter.write(" ");
             }
             bufferedWriter.newLine();
         } catch (IOException e) {
             throw new RuntimeException("Failed to write into file");
-
         }
-
-
     }
 }
