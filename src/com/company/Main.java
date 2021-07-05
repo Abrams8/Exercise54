60d67317da71c8c17d641040ff5957084a5f1854
Abrams <dima.abramchik@gmail.com>
2021-07-05 20:24:53 +0300

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Thread threadOne = new Thread(new MyRunnable());
-        Thread threadTwo = new Thread(new MyRunnable());
-        Thread threadThree = new Thread(new MyRunnable());
-        Thread threadFour = new Thread(new MyRunnable());
-        Thread threadFive = new Thread(new MyRunnable());
-
-        threadOne.start();
-        threadTwo.start();
-        threadThree.start();
-        threadFour.start();
-        threadFive.start();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(new MyRunnable());
+            thread.start();
+        }
     }
 }
